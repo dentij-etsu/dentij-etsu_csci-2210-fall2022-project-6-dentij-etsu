cb8b2dbf74ff8046ef8d307a1f5c4705543de338
Jackson Denti <dentij@ETSU.edu>
2022-11-21 00:31:10 -0500

    Add project files.

---
+++ dentij-etsu_csci-2210-fall2022-project-6-dentij-etsu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dentij_etsu_csci_2210_fall2022_project_6_dentij_etsu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
